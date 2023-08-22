1bce5aca9c5361b188b8fe6a582996479d2f69f6
manoel-linux <135872910+manoel-linux@users.noreply.github.com>
2023-08-22 16:32:01 -0300

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -2,7 +2,7 @@
 
 clear
 echo "#################################################################"
-echo "opti-sway-i3wm-uninstaller >> (aug 2023)"
+echo "(opti-sway-i3wm-uninstaller) >> (aug 2023)"
 echo "#################################################################"
 echo " ███████  ██████ ██████  ██ ██████  ████████ "
 echo " ██      ██      ██   ██ ██ ██   ██    ██    "
@@ -10,7 +10,7 @@ echo " ███████ ██      ██████  ██ ████
 echo "      ██ ██      ██   ██ ██ ██         ██    "
 echo " ███████  ██████ ██   ██ ██ ██         ██    "   
 echo "#################################################################"
-echo "opti-sway-i3wm-github >> (https://github.com/manoel-linux/opti-sway-i3wm)"
+echo "(opti-sway-i3wm-github) >> (https://github.com/manoel-linux/opti-sway-i3wm)"
 echo "#################################################################"
 
 if [[ $EUID -ne 0 ]]; then
